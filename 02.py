57b2445210ce7fcf3938e3573ef6f384833b2a1a
Wang <1558629882@qq.com>
2018-09-30 14:33:19 +0800

    03

---
+++ 02.py
@@ -0,0 +1,18 @@
+
+class Test01():
+
+    def test01(self):
+        print("test01被执行")
+    def test02(self):
+        print("test02被执行")
+    def test05(self):
+        print("test03被执行")
+
+
+
+
+
+
+
+
+
